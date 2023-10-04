ea04d639c5ea875097623908156a2b31d3169100
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-04 21:18:31 +0700

    Install Module react-router-dom and Setup Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Halo</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
