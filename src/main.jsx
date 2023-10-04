0fca7908b82af587f83e3a4d00456ca8d132f671
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-05 02:08:42 +0700

    Add Route Users

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/login";
+import UsersPage from "./pages/users";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginPage />,
+  },
+  {
+    path: "/users",
+    element: <UsersPage />,
   },
 ]);
 
