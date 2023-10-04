2fcb6521b6d50e9a6e0c1ed2fa7795d4193ba922
fahrooo <fahrooo2808@student.uns.ac.id>
2023-10-05 02:12:33 +0700

    Create Users Service

---
+++ src/services/users.service.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+export const getUsers = (callback) => {
+  axios
+    .get("http://localhost:5000/users")
+    .then((res) => callback(res.data))
+    .catch((err) => callback(err));
+};
+
+export const postUsers = (data, callback) => {
+  axios
+    .post("http://localhost:5000/users/create", data)
+    .then((res) => callback(res.data))
+    .catch((err) => callback(err));
+};
+
+export const putUsers = (id, data, callback) => {
+  axios
+    .put(`http://localhost:5000/users/update/${id}`, data)
+    .then((res) => callback(res.data))
+    .catch((err) => callback(err));
+};
+
+export const deleteUsers = (id, callback) => {
+  axios
+    .delete(`http://localhost:5000/users/update/${id}`)
+    .then((res) => callback(res.data))
+    .catch((err) => callback(err));
+};
